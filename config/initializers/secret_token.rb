35da24d94df3e8b160d62e396ec3e90dc9ae27ff
rn0 <rennoo@gmail.com>
2011-04-22 20:06:49 +0200

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MongoShop::Application.config.secret_token = 'd5227ea372e54938c77c0c729f20b1dd874fa1e4f0049b58cda4b0aca0c1960c3ebb2b08f4878d6d21fca55bd19ed77e400cda57456e6e287f75783180110344'
