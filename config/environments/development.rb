6760b85da44be267295ed1e099cefd2959a4d49c
rn0 <rennoo@gmail.com>
2011-06-12 20:03:48 +0200

    Slim not working with pretty mode

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -22,7 +22,7 @@ MongoShop::Application.configure do
   # Only use best-standards-support built into browsers
   config.action_dispatch.best_standards_support = :builtin
 
-  Slim::Engine.set_default_options :pretty => true
+  #Slim::Engine.set_default_options :pretty => true
   Tire.configure { logger 'log/elasticsearch.log' }
 end
 
